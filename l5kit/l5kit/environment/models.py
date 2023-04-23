df22016a2b9f05afcb52c203ca9901692972120a
Pronton2001 <trihuynhtk15@gmail.com>
2023-04-23 14:32:11 +0000

    update simplecnn model for 7x84x84 image

--- l5kit/l5kit/environment/models.py
+++ l5kit/l5kit/environment/models.py
@@ -8,6 +8,7 @@ def SimpleCNN_GN(num_input_channels: int, features_dim: int) -> nn.Module:
     :param features_dim: the number of features to extract from input
     """
     in_features = 1568 if num_input_channels == 7 else 6272 #7x122x122, 5x224x224 image
+    in_features = 800
     model = nn.Sequential(
         nn.Conv2d(num_input_channels, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False),
         nn.GroupNorm(4, 64),
